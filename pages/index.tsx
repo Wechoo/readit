d69eadcd9dbbdae876234a7cc3c46a9bc760aa90
MathPSantos <matheus.psantos2016@gmail.com>
2021-01-21 18:45:58 -0300

    feat: typescript added to project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div >
+      <Head>
+        <title>Read.it</title>
+      </Head>
+
+      <main >
+        Hello Read.it
+      </main>
+    </div>
+  )
+}
+
+export default Home
